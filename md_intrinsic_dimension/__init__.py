2d1892e90a06defe61d7ff19b3187883f38fed45
Toni Giorgino <toni.giorgino@gmail.com>
2025-11-06 16:20:04 +0100

    version attr

--- md_intrinsic_dimension/__init__.py
+++ md_intrinsic_dimension/__init__.py
@@ -1,6 +1,16 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from .md_intrinsic_dimension import intrinsic_dimension
 from .section_id import section_id
 from .secondary_structure_id import secondary_structure_id
+
+# TONI is this list correct?
 __all__ = ['md_intrinsic_dimension','section_id', 'secondary_structure_id']
 
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # Package is not installed, fallback (useful during local development)
+    __version__ = "0.0.0uninstalled"
 
